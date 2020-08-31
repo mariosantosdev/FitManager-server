58c5c5be0dc11fb1930534f83cce748cfcbd439f
nevr001 <playcar46@gmail.com>
2020-08-31 12:18:01 -0300

    Update logs.js
    Add try, catch

--- controllers/logs.js
+++ controllers/logs.js
@@ -2,14 +2,19 @@ const path = require('path')
 
 module.exports = app => {
     return (req, res) => {
-        if (req.params.type === 'error') {
-            res.sendFile(path.resolve('./logs/error.txt'))
-        } else if (req.params.type === 'info') {
-            res.sendFile(path.resolve('./logs/info.txt'))
-        } else if (req.params.type === 'debug') {
-            res.sendFile(path.resolve('./logs/debug.txt'))
-        } else {
-            res.status(400).json({ message: 'Esse tipo de log não existe no nosso sistema!' })
+        try {
+            if (req.params.type === 'error') {
+                res.sendFile(path.resolve('./logs/error.txt'))
+            } else if (req.params.type === 'info') {
+                res.sendFile(path.resolve('./logs/info.txt'))
+            } else if (req.params.type === 'debug') {
+                res.sendFile(path.resolve('./logs/debug.txt'))
+            } else {
+                res.status(400).json({ message: 'Esse tipo de log não existe no nosso sistema!' })
+            }
+        } catch (err) {
+            app.logger.error(err, __filename)
+            res.status(500).json({ message: 'Ocorreu um erro no servidor.', error: err })
         }
     }
 }
