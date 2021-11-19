7936b9f314015ddcf12e0ee317cea8a9f795836d
Mario Santos <mariodev7@gmail.com>
2021-11-19 13:18:51 -0300

    feat(recovery password): added route to recovery password

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import signUpController from '@controllers/authControllers/signUp.controller';
 import signInController from '@controllers/authControllers/signIn.controller';
+import forgetPassword from '@controllers/authControllers/forgetPassword.controller';
 import tokenController from '@controllers/tokenControllers/refreshToken.controller';
 import middlewareAuth from "./middlewares/ensureAuth";
 
@@ -16,5 +17,6 @@ routes.post('/newToken/:token', tokenController.handle)
 // Authentication Routes
 routes.post('/signup', signUpController.handle);
 routes.post('/signin', signInController.handle);
+routes.post('/forgetPassword', forgetPassword.handle);
 
 export default routes;
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,10 +1,18 @@
 import { PrismaClient, User } from "@prisma/client";
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
+import nodemailer from 'nodemailer';
 
 interface ICreateUser {
     name: string;
     email: string;
     password: string;
+}
+
+interface IDataSendMail {
+    from: string;
+    to: string;
+    subject: string;
+    html: string | Buffer;
 }
 
 class AuthServices {
@@ -50,6 +58,67 @@ class AuthServices {
                 reject(error);
             }
         });
+    }
+
+    generatePasswordRecovered() {
+        return Math.random().toString(36).slice(-8);
+    }
+
+    forgetPassword(email: string) {
+        return new Promise<Boolean>(async (resolve, reject) => {
+            try {
+                // Verify is exist user
+                const user = await this.prisma.user.findFirst({
+                    where: { email }
+                });
+                if (!user) reject('User not found.')
+
+                const newPassword = this.generatePasswordRecovered();
+                const hashedPassword = await this.generatePasswordHash(newPassword);
+
+                await this.prisma.user.update({
+                    where: { id: user.id },
+                    data: {
+                        password: hashedPassword,
+                        updated_at: new Date()
+                    }
+                });
+
+                await this.sendMail({
+                    from: process.env.EMAIL,
+                    to: email,
+                    subject: 'Recuperação de senha Fit Manager',
+                    html: `Olá ${user.name} sua nova senha para acessar o app Fit Manager é <b>${newPassword}<b>`
+                });
+
+                resolve(true);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
+
+    sendMail(data: IDataSendMail) {
+        return new Promise<Boolean>((resolve, reject) => {
+            try {
+                const transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    secure: true,
+                    auth: {
+                        user: process.env.EMAIL,
+                        pass: process.env.PASSWORD_EMAIL
+                    }
+                });
+
+                transporter.sendMail(data, (err) => {
+                    if (err) throw err;
+
+                    resolve(true);
+                })
+            } catch (error) {
+                reject(error);
+            }
+        })
     }
 
     generatePasswordHash(password: string) {
