887acb0ccaf456648a72698f25dcca368ce2197e
Mario Santos <mariodev7@gmail.com>
2021-12-22 18:26:12 -0300

    feat(cors): setup cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 import routes from '@source/routes';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
